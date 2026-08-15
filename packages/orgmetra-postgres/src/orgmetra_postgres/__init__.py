d50c916e092f28b7e416151a53c78766b50ba1ad
Seongho Bae <me@seonghobae.me>
2026-08-15 19:36:29 +0900

    feat(postgres): publish repository API

---
+++ packages/orgmetra-postgres/src/orgmetra_postgres/__init__.py
@@ -0,0 +1,24 @@
+"""Public API for Orgmetra's purpose-bound PostgreSQL adapter."""
+
+from .context import PurposeContext
+from .errors import (
+    RepositoryAuthorizationError,
+    RepositoryConflictError,
+    RepositoryError,
+    RepositoryUnavailableError,
+)
+from .models import AuditEvent, CandidateSnapshot, CandidateWorkerLink, PersonSnapshot
+from .repository import PostgresPeopleRepository
+
+__all__ = [
+    "AuditEvent",
+    "CandidateSnapshot",
+    "CandidateWorkerLink",
+    "PersonSnapshot",
+    "PostgresPeopleRepository",
+    "PurposeContext",
+    "RepositoryAuthorizationError",
+    "RepositoryConflictError",
+    "RepositoryError",
+    "RepositoryUnavailableError",
+]
