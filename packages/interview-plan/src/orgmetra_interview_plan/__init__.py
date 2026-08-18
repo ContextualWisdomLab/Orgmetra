12566d6ca0e618b5c7d5cb6442a86af4ce7821c3
Seongho Bae <me@seonghobae.me>
2026-08-18 07:06:25 -0700

    feat: export structured interview plan contract

---
+++ packages/interview-plan/src/orgmetra_interview_plan/__init__.py
@@ -0,0 +1,4 @@
+"""Public structured-interview planning contracts for Orgmetra."""
+from .plan import StructuredInterviewPlan, build_structured_interview_plan
+
+__all__ = ["StructuredInterviewPlan", "build_structured_interview_plan"]
