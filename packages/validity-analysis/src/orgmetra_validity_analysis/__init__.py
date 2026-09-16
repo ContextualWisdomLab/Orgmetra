7afa6ce3473bb2a178d55e1704e848d266d43a30
Seongho Bae <me@seonghobae.me>
2026-09-17 07:05:18 +0900

    feat(validity): export weight eligibility receipt

--- packages/validity-analysis/src/orgmetra_validity_analysis/__init__.py
+++ packages/validity-analysis/src/orgmetra_validity_analysis/__init__.py
@@ -12,6 +12,7 @@ from .weights import (
     FinalAnalysisWeightReceipt,
     NonresponseAdjustmentReceipt,
     TrimmingBoundingAdjustmentReceipt,
+    WeightEligibilityReceipt,
 )
 
 __all__ = [
@@ -26,4 +27,5 @@ __all__ = [
     "FinalAnalysisWeightReceipt",
     "NonresponseAdjustmentReceipt",
     "TrimmingBoundingAdjustmentReceipt",
+    "WeightEligibilityReceipt",
 ]
