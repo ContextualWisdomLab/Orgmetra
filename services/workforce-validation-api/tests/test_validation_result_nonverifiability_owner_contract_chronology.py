dede2a2c885404aa7b309b60545e9f1a8a03b766
Seongho Bae <me@seonghobae.me>
2026-09-18 06:09:46 +0900

    test(workforce-validation): align owner chronology with attempt release

--- services/workforce-validation-api/tests/test_validation_result_nonverifiability_owner_contract_chronology.py
+++ services/workforce-validation-api/tests/test_validation_result_nonverifiability_owner_contract_chronology.py
@@ -17,6 +17,7 @@ TENANT = UUID("10000000-0000-7000-8000-000000000001")
 STUDY = UUID("00000000-0000-7000-8000-0000000000d1")
 OWNER_CONTRACT_RELEASED_AT = datetime(2026, 9, 17, 5, 55, tzinfo=timezone.utc)
 EVALUATED_AT = datetime(2026, 9, 17, 6, 0, tzinfo=timezone.utc)
+ATTEMPT_RELEASED_AT = datetime(2026, 9, 17, 6, 2, tzinfo=timezone.utc)
 RELEASED_AT = datetime(2026, 9, 17, 6, 5, tzinfo=timezone.utc)
 
 
@@ -37,6 +38,7 @@ def _record(**overrides: object) -> ValidationResultNonVerifiabilityRecord:
             "33333333-3333-4333-8333-333333333333"
         ),
         "verification_attempt_digest": "3" * 64,
+        "verification_attempt_released_at": ATTEMPT_RELEASED_AT,
         "owner_contract_reference": (
             "released_owner_contract:44444444-4444-4444-8444-444444444444"
         ),
